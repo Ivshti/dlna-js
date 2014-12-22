b7ebd6511bd5be0a8f75a7a86d8084729fd376e6
wallydz <wallydz@get-popcorn.com>
2014-12-22 02:17:53 +0100

    Initial Commit

---
+++ examples/example.js
@@ -0,0 +1,9 @@
+var UpnpControlPoint = require("../lib/upnp-controlpoint").UpnpControlPoint;
+
+var handleDevice = function(device) {
+	console.log("device type: " + device.deviceType + " location: " + device.location + device.friendlyName);
+	console.log(device);
+}
+var cp = new UpnpControlPoint();
+cp.on("device", handleDevice);
+cp.search();
